f66162281f7155fab3234cacd014c112cbf0b048
Arno Appenzeller <arno@Arnos-iMac-2.local>
2014-11-12 16:42:35 +0100

    Added new method to check on which type of connection you are

--- Swift-Reachability/Reachability.swift
+++ Swift-Reachability/Reachability.swift
@@ -9,6 +9,12 @@
 import Foundation
 import SystemConfiguration
 
+enum ReachabilityType {
+    case WWAN,
+    WiFi,
+    NotConnected
+}
+
 public class Reachability {
     
     /**
@@ -33,6 +39,40 @@ public class Reachability {
         let needsConnection = (flags & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
         
         return (isReachable && !needsConnection) ? true : false
+    }
+    
+    class func isConnectedToNetworkOfType() -> ReachabilityType {
+        
+        var zeroAddress = sockaddr_in(sin_len: 0, sin_family: 0, sin_port: 0, sin_addr: in_addr(s_addr: 0), sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
+        zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        
+        
+        let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
+            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0)).takeRetainedValue()
+        }
+        
+        var flags: SCNetworkReachabilityFlags = 0
+        if SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) == 0 {
+            return .NotConnected
+        }
+        
+        let isReachable = (flags & UInt32(kSCNetworkFlagsReachable)) != 0
+        let isWWAN = (flags & UInt32(kSCNetworkReachabilityFlagsIsWWAN)) != 0
+        //let isWifI = (flags & UInt32(kSCNetworkReachabilityFlagsReachable)) != 0
+        
+        if(isReachable && isWWAN){
+            return .WWAN
+        }
+        if(isReachable && !isWWAN){
+            return .WiFi
+        }
+        
+        return .NotConnected
+        //let needsConnection = (flags & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        
+        //return (isReachable && !needsConnection) ? true : false
     }
     
 }
