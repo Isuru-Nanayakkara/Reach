3712c3de856c5b8c728dc1665d834b44e969d21e
Isuru Nanayakkara <contact.isuru@gmail.com>
2015-10-30 20:13:29 +0530

    New beginnings

---
+++ README.md
@@ -0,0 +1,26 @@
+IJReachability
+==================
+
+A simple class to check for internet connection availability in Swift. Works for both 3G and WiFi connections.
+
+
+## Install
+
+##### [CocoaPods](http://cocoapods.org/) (< v0.36)
+- Create a `Podfile`.
+- Add the line, `pod 'IJReachability', :git => 'https://github.com/Isuru-Nanayakkara/IJReachability.git'`.
+- Run `pod install`.
+- Add `import IJReachability` to the files where you want to use the library. 
+
+
+##### Manually
+- Add the *IJReachability.swift* file to your project.
+- Add the *SystemConfiguration* framework.
+
+
+## Credits
+
+* [Chris Danielson](http://www.chrisdanielson.com/2009/07/22/iphone-network-connectivity-test-example/) is the author of the original code written in Objective-C.
+* [Martin R](http://stackoverflow.com/users/1187415/martin-r) from StackOverflow helped me immensely in converting C code to Swift.
+
+
---
+++ Reach-swift1.2/Reach.swift
@@ -0,0 +1,95 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Isuru Nanayakkara
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+
+import Foundation
+import SystemConfiguration
+
+
+enum ReachabilityType: Printable {
+    case WWAN
+    case WiFi
+    
+    var description: String {
+        switch self {
+        case .WWAN: return "WWAN"
+        case .WiFi: return "WiFi"
+        }
+    }
+}
+
+enum ReachabilityStatus: Printable  {
+    case Offline
+    case Online(ReachabilityType)
+    case Unknown
+    
+    var description: String {
+        switch self {
+        case .Offline: return "Offline"
+        case .Online(let type): return "Online (\(type))"
+        case .Unknown: return "Unknown"
+        }
+    }
+}
+
+
+/**
+:see: Original post - http://www.chrisdanielson.com/2009/07/22/iphone-network-connectivity-test-example/
+*/
+public class Reach {
+    
+    func connectionStatus() -> ReachabilityStatus {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
+            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0)).takeRetainedValue()
+        }
+        
+        var flags: SCNetworkReachabilityFlags = 0
+        if SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) == 0 {
+            return .Unknown
+        }
+        
+        return ReachabilityStatus(reachabilityFlags: flags)
+    }
+    
+}
+
+extension ReachabilityStatus {
+    private init(reachabilityFlags flags: SCNetworkReachabilityFlags) {
+        let connectionRequired = (flags & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        let isReachable = (flags & UInt32(kSCNetworkFlagsReachable)) != 0
+        let isWWAN = (flags & UInt32(kSCNetworkReachabilityFlagsIsWWAN)) != 0
+        
+        if !connectionRequired && isReachable {
+            if isWWAN {
+                self = .Online(.WWAN)
+            } else {
+                self = .Online(.WiFi)
+            }
+        } else {
+            self =  .Offline
+        }
+    }
+}
